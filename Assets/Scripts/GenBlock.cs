0971f58300a15a1686e55ffa9002ebe6b49643a2
ry0 <tan0ry0shiny@gmail.com>
2020-11-25 08:50:38 +0900

    block drop
    when starting

---
+++ Assets/Scripts/GenBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenBlock : MonoBehaviour
+{
+    public Object Wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(wall, new Vector3(5.0f * x, 5.0f, 5.0f * z), Quaternion.identity);
+        Instantiate(Wall, new Vector3(0, 5.0f, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
